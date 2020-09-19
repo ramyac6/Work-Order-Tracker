214903f39e83cc13b10506f44e4996f06bca37e2
Ramya Challa <ramyac6@cs.washington.edu>
2020-09-19 08:41:08 -0700

    created color palette

---
+++ src/constants/Colors.js
@@ -0,0 +1,12 @@
+export default {
+  lightGray: "#efefef",
+  gray: "#a0a0a0",
+  darkGray: "#333333",
+  lightBlue: "#94ded0",
+  darkBlue: "#1f404c",
+  //paleBlue: "#bad5ff",
+  //blue: "#4d92ff",
+  //seaBlue: "#003ea0",
+  //lightPurple: "#cfc6f9",
+  //royalBlue: "#4169E1",
+};
